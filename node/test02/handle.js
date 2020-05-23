486899ee5982e7522ba737ad8277c5e0c2cc4e39
furry <18834176546@163.com>
2020-05-23 22:48:43 +0800

    模拟阻塞操作

--- node/test02/handle.js
+++ node/test02/handle.js
@@ -2,13 +2,21 @@ let handle={};
       
 function login(){
     console.log('欢迎，请先登录/注册');
+    // 阻塞操作
+    function sleep(millSeconds) {
+        let startTime=new Date().getTime();
+        while (new Date().getTime()<(startTime+millSeconds)) ;
+        return '我醒啦~';
+    }
+    sleep(10000);
 }
-function user(name){
-    console.log('你好，'+name);
+function user(){
+    console.log('你好，');
+    return '你好，';
 }
 
 handle['/']=login;
 handle['/login']=login;
-handle['/user?name='+name]=user;
+handle['/user']=user;
 
 exports.handle=handle;
--- node/test02/server.js
+++ node/test02/server.js
@@ -10,6 +10,7 @@ function start(handle){
         res.writeHead(200, {"Content-Type": "text/plain"});
         res.write("Hello World");
         res.end();
+        
     }
     http.createServer(onRequest).listen(8888);
     console.log('服务已启动~');
