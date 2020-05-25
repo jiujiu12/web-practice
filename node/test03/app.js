9b8b8d70394b588c0e5b67d7e6a1d48fc7f8da04
furry <18834176546@163.com>
2020-05-25 23:11:33 +0800

    博客后台练习

--- node/test03/app.js
+++ node/test03/app.js
@@ -2,11 +2,43 @@ const http=require('http');
 const queryString=require('querystring');
 
 const server=http.createServer((req,res)=>{
-    console.log("methods:"+req.method);
+    const method=req.method;
     const url=req.url;
-    console.log("url:"+url);
-    req.query=queryString.parse(url.split('?')[1]);
-    console.log(req.query);
-    res.end=(JSON.stringify(req.query));
-    
-}).listen(8000);
+    const path=url.split('?')[0];
+    const query=queryString.parse(url.split('?')[1]);
+    const env=process.env.NODE_ENV;
+
+    // 设置返回数据类型
+    res.setHeader('Content-type','application/json');
+    res.write("query:"+JSON.stringify(query))
+
+    // 返回的数据
+    const resData={
+        method,
+        url,
+        path,
+        query,
+        env
+    }
+
+    console.log(JSON.stringify(req.query));
+    // get方法：
+    if(req.method==='GET'){
+        res.end(JSON.stringify(resData));
+    }
+    //post方法
+    else if (req.method==='POST'){
+        console.log('req contentType:'+req.headers['content-type']);
+        let postData='';
+        req.on('data',chunk=>{
+            postData+=chunk.toString();
+        })
+        req.on('end',()=>{
+            console.log(postData);
+            resData.postData=postData;
+            res.end(JSON.stringify(resData));
+        })
+    }
+})
+server.listen(8000);
+console.log("ok")
