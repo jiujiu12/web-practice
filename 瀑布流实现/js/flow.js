9f931005718827216fd00c7bf9530e230ff9c89a
furry <18834176546@163.com>
2020-05-29 23:55:12 +0800

    调整添加js实现

--- 瀑布流实现/js/flow.js
+++ 瀑布流实现/js/flow.js
@@ -1,5 +1,21 @@
-$(document).ready(function(){
-    var lists=$(".item").attr("height");
-    console.log(lists);
-    
+$(function(){
+    warterfall();
+    var heightArr=[];
+    $(".item").each(function (index) {
+        heightArr[index]=$(this).outerHeight();
+    })
+
  });
+//  页面重加载事件
+ $(window).resize(function () {
+     warterfall();
+ })
+//  计算所需列数 函数
+ function countCol() {
+     var colNum=Math.floor($(".wrapper").outerWidth/$(".item").outerWidth);
+     return colNum;
+ }
+
+ function waterfall() {
+     
+ }
